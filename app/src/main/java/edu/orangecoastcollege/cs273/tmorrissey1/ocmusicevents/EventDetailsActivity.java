e90710651ff07691089a94bb39d220e8258f022e
tmorrissey1 <tmorrissey1@student.cccd.edu>
2016-09-27 16:28:18 -0700

    Created files

--- app/src/main/java/edu/orangecoastcollege/cs273/tmorrissey1/ocmusicevents/EventDetailsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/tmorrissey1/ocmusicevents/EventDetailsActivity.java
@@ -1,13 +1,46 @@
 package edu.orangecoastcollege.cs273.tmorrissey1.ocmusicevents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class EventDetailsActivity extends AppCompatActivity {
+
+    private ImageView eventImageView;
+    private Context context = (Context) this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+
+
+        Intent detailsIntent = getIntent();
+        String title = detailsIntent.getStringExtra("Title");
+        String details = detailsIntent.getStringExtra("Details");
+        String imageFileName = title.replace(" ", "") + ".jpeg";
+
+        eventImageView = (ImageView) findViewById(R.id.eventImageView);
+        AssetManager am = context.getAssets();
+
+        try {
+            InputStream stream = am.open(imageFileName);
+            Drawable image = Drawable.createFromStream(stream, title);
+            eventImageView.setImageDrawable(image);
+        }
+        catch (IOException e) {
+            Log.e("OC Music Events", "Cannot load image: " + imageFileName + e);
+
+        }
+
     }
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/tmorrissey1/ocmusicevents/EventListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/tmorrissey1/ocmusicevents/EventListActivity.java
@@ -1,14 +1,32 @@
 package edu.orangecoastcollege.cs273.tmorrissey1.ocmusicevents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class EventListActivity extends AppCompatActivity {
+public class EventListActivity extends ListActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_list);
+
+        // Set the adapter which binds the listView with the data in MusicView.java
+        // Since the data is in an array , we use an ArrayAdapter
+
+        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, MusicEvent.titles));
+
+        //setContentView(R.layout.activity_event_list);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+        String title = MusicEvent.titles[position];
+        String details = MusicEvent.details[position];
+
+
     }
 }
